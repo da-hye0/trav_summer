37242995642a1473e6a6ff93625cf144ef0eadf3
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2023-06-05 17:06:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+    <a href="https://link.coupang.com/a/0flOq" target="_blank" referrerpolicy="unsafe-url"><img src="https://ads-partners.coupang.com/banners/667338?subId=&traceId=V0-301-bae0f72e5e59e45f-I667338&w=320&h=50" alt=""/></a>
         <br/><br/><br/>
           <a className = "mainfont " >
             나만의 힐링스팟
@@ -37,10 +38,10 @@ function App() {
           </Link>
           <br/> <br/>
               <a className = "desc">CUBEE</a>
-          <br/>
            <a href="https://link.coupang.com/a/0fiSu" target="_blank" referrerpolicy="unsafe-url">
                    <img src="https://ads-partners.coupang.com/banners/667327?subId=&traceId=V0-301-371ae01f4226dec2-I667327&w=320&h=50" alt=""/>
             </a>
+            <br/>
 
       </header>
     </div>
