9d1ef2f4a63842e2c8fc0fd4c3826cb737d202d6
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2023-06-05 17:19:07 +0900

    Update lastUI

--- src/Question.js
+++ src/Question.js
@@ -93,6 +93,7 @@ function Question(){
   return(   
     <div className="App">
       <header className="App-header">
+     <br/>
      <a href="https://link.coupang.com/a/0flOq" target="_blank" referrerpolicy="unsafe-url"><img src="https://ads-partners.coupang.com/banners/667338?subId=&traceId=V0-301-bae0f72e5e59e45f-I667338&w=320&h=50" alt=""/></a>
           <head>
               <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5741659942476200"
@@ -183,9 +184,11 @@ function Question(){
                               <a className = "desc"> • {id.desc[1]}  <br/><br/> </a>
                               <a className = "desc"> • {id.desc[2]}  <br/><br/> </a>
                               <a className = "desc"> • {id.desc[3]}  <br/><br/> </a>
-                            <br/><br/> <br/>
+                            <br/><br/> 
+                                 <a href="https://link.coupang.com/a/0fmwN" target="_blank" referrerpolicy="unsafe-url"><img src="https://ads-partners.coupang.com/banners/667353?subId=&traceId=V0-301-5a8c79a76485eb21-I667353&w=320&h=50" alt=""/></a><br/>
                             <a className = "desc">CUBEE</a>
                             <br/> <br/>
+                               
                                <br/> <br/> 
                           </div> 
                           
@@ -201,7 +204,6 @@ function Question(){
                       <div className="App">
                       <header className="App-header">
                         <br/><br/><br/>
-                     <a href="https://link.coupang.com/a/0flOq" target="_blank" referrerpolicy="unsafe-url"><img src="https://ads-partners.coupang.com/banners/667338?subId=&traceId=V0-301-bae0f72e5e59e45f-I667338&w=320&h=50" alt=""/></a>
                           <a className = "mainfont " >
                             테스트 완료!
                           </a>
@@ -212,12 +214,13 @@ function Question(){
                             결과확인하기
                           </button>
                          
-                          <br/> <br/>
+                          <br/>
                               <a className = "desc">CUBEE</a>
-                         
+                      
                             <a href="https://link.coupang.com/a/0fmwN" target="_blank" referrerpolicy="unsafe-url"><img src="https://ads-partners.coupang.com/banners/667353?subId=&traceId=V0-301-5a8c79a76485eb21-I667353&w=320&h=50" alt=""/></a>
-                           <br/><br/> <br/>
+                           <br/><br/>
                       </header>
+
                   </div>
                   </>
                   )}
